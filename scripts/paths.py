6be962f07dee1e7a344167dd026d004678de9b12
Sebastian Latham <sebastian.tlatham@gmail.com>
2025-11-14 20:12:17 +0000

    fixed paths.py script so that it uses full path meaning it can be called from inside of scripts or in the root

--- scripts/paths.py
+++ scripts/paths.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 def plot_paths_from_csv(filename):
     df = pd.read_csv(filename, header=None)
@@ -21,4 +22,10 @@ if __name__ == "__main__":
         print("Usage: python plot_paths.py <filename>")
     else:
         filename = sys.argv[1]
-        plot_paths_from_csv("data/" + filename)
+
+        script_path = os.path.abspath(__file__)
+        script_dir = os.path.dirname(script_path)
+        project_root = os.path.abspath(os.path.join(script_dir, '..'))
+        full_path = os.path.join(project_root, "data", filename)
+
+        plot_paths_from_csv(full_path)
